191cdbb908046ed4a00e00d018cd80d7627747dc
Abhinav Raj <abhinav.raj0509@gmail.com>
2021-08-01 18:26:43 +0530

    Create Articles.js
    intial commit

---
+++ src/components/Articles.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+function Articles({ articles, sortField }) {
+  //console.log(articleList);
+
+  return (
+    <div className="card w-50 mx-auto">
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Upvotes</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {articles.map((article) => {
+            return (
+              <tr data-testid="article" key={article.title}>
+                <td data-testid="article-title">{article.title}</td>
+                <td data-testid="article-upvotes">{article.upvotes}</td>
+                <td data-testid="article-date">{article.date}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Articles;
